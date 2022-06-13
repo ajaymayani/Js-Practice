e43981731261d09a491367415d91fdca7897426c
ajaymayani <ajaymayani96@gmail.com>
2022-06-13 18:05:39 +0530

    change corona virus case api

--- js/index.js
+++ js/index.js
@@ -1,29 +1,34 @@
-function updateMap(){
+let dt = document.getElementById('dt');
+
+function updateMap() {
     console.log("update map function is called");
 
-    fetch("../data.json")
-    .then(response => response.json())
-    .then(rsp =>{
-        rsp.data.forEach(element => {
-            longitude = element.longitude;
-            latitude = element.latitude;
-            cases = element.infected;
-            if(cases > 255)
-            {
-                color = "rgb(255,0,0)"
-            }else{
-                color = `rgb(${cases},0,0)`
-            }
+    fetch("https://corona-api.com/countries?include=1")
+        .then(response => response.json())
+        .then(rsp => {
+            rsp.data.forEach(element => {
+                longitude = element.coordinates.longitude;
+                latitude = element.coordinates.latitude;
 
-            new mapboxgl.Marker({
-                draggable : true,
-                color : color
-            })
-            .setLngLat([longitude,latitude])
-            .addTo(map)
+                cases = element.latest_data.critical;
 
-        });
-    })
+                dt.innerHTML = "Updated At : "+ element.updated_at.substring(0,10);
+
+                if (cases > 255) {
+                    color = "rgb(255,0,0)"
+                } else {
+                    color = `rgb(${cases},0,0)`
+                }
+
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map)
+
+            });
+        })
 }
 
 updateMap();
